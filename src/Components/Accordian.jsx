b3b382d9b5380cb23d484ddf2147794f0a819230
Mahadevan Shettiyar <mahadevanshettiyar@gmail.com>
2025-05-10 12:38:32 +0530

    removed the curly img from accordian

--- src/Components/Accordian.jsx
+++ src/Components/Accordian.jsx
@@ -5,8 +5,6 @@ import "../Components/Accordian.css";
 const Accordian = ({ data }) => {
     return (
       <div className="faq-wrapper">
-        <img src="../Images/Faq.jpg" className="curly-left" alt="" />
-        <img src="../Images/Faq.jpg" className="curly-right" alt="" />
         <h2 className="faq-title">Password Generator FAQs</h2>
         <div className="faq-grid">
           {data.map((item, index) => (
